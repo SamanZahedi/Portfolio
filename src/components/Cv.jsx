971820cce9355e1536dc3583c2300cc7378be8c2
Saman <saman.zahedi10@gmail.com>
2022-06-05 23:47:44 +0100

    target=new window

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -4,28 +4,7 @@ const Cv = () => {
   return (
     <main>
         <h2>My CV</h2>
-        <div>
-            <p></p>
-        </div>
-      Saman Zahedi London saman_zah@yahoo.co.uk 07479 219 766 PROFILE A
-      well-presented, hardworking, enthusiastic, honest, reliable,
-      self-disciplined, self-motivated, proactive, quick learner, punctual, an
-      approachable person and able to work in a team or on own initiative, with
-      excellent communication skills both written and oral. KEY SKILLS     
-      IT – Technical proficiency in Word, Excel and PowerPoint. Time-keeping –
-      Excellent time keeper and always being punctual Teamwork – A very
-      constructive team member who contributes practically to ensure the success
-      of the team Commitment – Very energetic individual whose enthusiasm in
-      pursuing tasks is second to none Languages – Fluent in Kurdish and
-      Persian. WORK EXPERIENCE Accounts Assistant – Via Email
-      for Millwood Accounting (www.millwoodaccounting.com) February 2019 – July
-      2019           Preparing financial documents such as invoices,
-      bills, and accounts payable and receivable Completing purchase orders
-      Managing payroll Completing financial reports on a regular basis and
-      providing information to the finance team Assisting with budgets
-      Completing bank reconciliations Entering financial information into
-      appropriate software programs Managing company ledgers Processing business
-      expenses Coordinating internal and external audits  References Available on Request
+      
     </main>
   );
 };
